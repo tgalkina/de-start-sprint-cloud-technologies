a0a0db1e866b17d436a5984fdb20889bb3afe2ef
Татьяна <tgalkina1993@yandex.ru>
2025-10-29 21:19:09 +0300

    Update app.py

--- solution/service_cdm/src/app.py
+++ solution/service_cdm/src/app.py
@@ -1,31 +1,39 @@
 import logging
-
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
 
 from app_config import AppConfig
 from cdm_loader.cdm_message_processor_job import CdmMessageProcessor
-
+from cdm_loader.repository.cdm_repository import CdmRepository
 
 app = Flask(__name__)
 
 config = AppConfig()
 
-
 @app.get('/health')
 def hello_world():
     return 'healthy'
 
-
 if __name__ == '__main__':
-    app.logger.setLevel(logging.DEBUG)
+    app.logger.setLevel(logging.INFO)
+    
+    # Инициализация компонентов
+    consumer = config.kafka_consumer()
+    pg_connect = config.pg_warehouse_db()
+    cdm_repository = CdmRepository(pg_connect)
 
-    proc = CdmMessageProcessor(
-        app.logger
+    # Инициализация процессора
+    processor = CdmMessageProcessor(
+        consumer=consumer,
+        cdm_repository=cdm_repository,
+        db=pg_connect,
+        logger=app.logger
     )
 
+    # Запуск планировщика
     scheduler = BackgroundScheduler()
-    scheduler.add_job(func=proc.run, trigger="interval", seconds=25)
+    scheduler.add_job(func=processor.run, trigger="interval", seconds=25)
     scheduler.start()
 
-    app.run(debug=True, host='0.0.0.0', use_reloader=False)
+    app.logger.info("CDM Service started successfully")
+    app.run(debug=False, host='0.0.0.0', port=5000, use_reloader=False)
